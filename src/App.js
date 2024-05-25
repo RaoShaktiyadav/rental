0fe5d6bc84e75fa88d37c2e8e93eb8f5dfd5b1d9
shakti <shaktiyadav635@gmail.com>
2024-05-26 02:20:34 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./component/Home.css";
+import logo from './component/img/logo.jpg'
+import React, { Component } from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
+import Home from "./component/Home";
+import About from "./component/About";
+import Contact from "./component/Contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div className="App">
+               <div className="imaged">
+                            <img className='i1' src={logo}  />
+                           
+                            </div>
+                    <ul className="App-header">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/Cars">
+                                Cars
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/Blog">
+                                Blog
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/About">
+                                About 
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/Contact">
+                                Contact Us
+                            </Link>
+                        </li>
+                    
+                    </ul>
+                    
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={<Home />}
+                        ></Route>
+                        <Route
+                            path="/about"
+                            element={<About />}
+                        ></Route>
+                        <Route
+                            path="/contact"
+                            element={<Contact />}
+                        ></Route>
+                    </Routes>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home() {
+    return (
+    <div className="hback">
+     <section id="hero">
+        <h4>Cars For Rental</h4>
+        <h2>Affordable price</h2>
+        <h1>On all Cars</h1>
+        <p>save more coupons &amp; up to 5% off!</p>
+        <button>Book now</button>
+      </section>
+    </div>
+
+    )
+}
+
+export default Home;
