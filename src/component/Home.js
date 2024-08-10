043f8a6f61121ee0f8a908d9dae3343d9415e13e
shakti <shaktiyadav635@gmail.com>
2024-08-10 13:20:28 +0530

    s

--- src/component/Home.js
+++ src/component/Home.js
@@ -79,7 +79,7 @@ const  Home = ({ phoneNumber, message }) => {
     };
 
     const Dail = () => {
-      const phoneNumber = "+1234567890";
+      const phoneNumber = "+918901636636";
       if (navigator.userAgent.match(/Mobi/)) {
         // It's a mobile device
         window.location.href = `tel:${phoneNumber}`;
